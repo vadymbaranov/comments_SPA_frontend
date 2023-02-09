734bc0651ff56de4dd19474d676a39c0b670a5ad
Vadym Baranov <vadym.baranov@hotmail.com>
2023-02-09 15:25:05 +0200

    testing json config

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -24,7 +24,7 @@ function request<T>(
   if (data) {
     options.body = new URLSearchParams(data).toString();
     options.headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json; charset=UTF-8',
       'Access-Control-Allow-Origin': '*',
     };
   }
